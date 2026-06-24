57b602a7148b62026bc688508fef3413adc9a40a
Varga Bence Csaba <vargabencecsaba2005@gmail.com>
2026-06-24 12:50:09 +0200

    added extra params to save with model weights & biases

--- model/module.py
+++ model/module.py
@@ -1,19 +1,41 @@
 import torch
 
 
+def make_layers(in_features: int, out_features: int) -> torch.nn.Module:
+    return torch.nn.Sequential(
+        torch.nn.Linear(in_features, 200),
+        torch.nn.ReLU(),
+        torch.nn.Linear(200, 50),
+        torch.nn.ReLU(),
+        torch.nn.Linear(50, out_features)
+    )
+
+
+def load_cover_model(path: str) -> CoverModel:
+    save_data = torch.load(path)
+    model = CoverModel(save_data["in_features"], save_data["out_features"])
+    model.load_state_dict(save_data["state_dict"])
+    return model
+
+
 class CoverModel(torch.nn.Module):
 
     def __init__(self, input_size: int, output_size: int):
         super().__init__()
 
-
-        self.linear_stack = torch.nn.Sequential(
-            torch.nn.Linear(input_size, 200),
-            torch.nn.ReLU(),
-            torch.nn.Linear(200, 50),
-            torch.nn.ReLU(),
-            torch.nn.Linear(50, output_size)
-        )
+        self.in_features = input_size
+        self.out_features = output_size
+        self.linear_stack = make_layers(self.in_features, self.out_features)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.linear_stack(x)
+
+    def save_cover_model(self, path: str):
+        save_data = {
+            "state_dict": self.state_dict(),
+            "in_features": self.in_features,
+            "out_features": self.out_features
+        }
+        torch.save(save_data, path)
+        print(f"Saved model to {path}")
+
--- model/train.py
+++ model/train.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 
 import torch
+from torch.distributed.checkpoint import state_dict
 
 import util.const as const
-from model.module import CoverModel
+from model.module import CoverModel, load_cover_model
 
 
-def train_epochs(model: torch.nn.Module, x: torch.Tensor, y_true: torch.Tensor, epochs: int):
+def train_epochs(model: CoverModel, x: torch.Tensor, y_true: torch.Tensor, epochs: int):
 
     optimizer = torch.optim.Adam(model.parameters(), lr=const.LEARN_RATE)
     loss_fn = torch.nn.MSELoss()
@@ -22,8 +23,7 @@ def train_epochs(model: torch.nn.Module, x: torch.Tensor, y_true: torch.Tensor, 
         if epoch % 10 == 0:
             print(f"Epoch: {epoch:02d}, Loss: {loss.item():.4f}")
 
-    torch.save(model.state_dict(), './data/model.pt')
-    print(f"Saved model to ./data/model.pt")
+    model.save_cover_model("./data/model.pt")
 
 
 def test_model(x_test: torch.Tensor, y_true_test: torch.Tensor) -> float:
@@ -32,9 +32,7 @@ def test_model(x_test: torch.Tensor, y_true_test: torch.Tensor) -> float:
     if not file.exists():
         raise FileNotFoundError(f"File {file} not found. Did you forget to train the model?")
 
-    prod_model = CoverModel(x_test.shape[1], y_true_test.shape[1])
-    prod_model.load_state_dict(torch.load('./data/model.pt'))
-    prod_model.eval()
+    prod_model = load_cover_model(str(file))
 
     with torch.no_grad():
         y_pred = prod_model(x_test)
